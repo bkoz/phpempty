ba033eefc88df1303536e523d2de0dd7592c5fb9
Bob Kozdemba <bkozdemba@gmail.com>
2016-02-15 10:33:54 -0600

    Update index.php
    Added openshift env vars

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-# place holder
+<?php
+	// set your infomation.
+	$host		=	$_ENV["MYSQL_SERVICE_HOST"];
+	$user		=	$_ENV["MYSQL_USER"];
+	$pass		=	$_ENV["MYSQL_PASSWORD"];
+	$database	=	$_ENV["MYSQL_DATMYSQL_SERVICE_HOST"];
+
+	// connect to the mysql database server.
+	$connect = @mysql_connect ($host, $user, $pass);
+
+	if ( $connect )
+	{
+		// create the database.
+		if ( ! @mysql_query ( "CREATE DATABASE `$database`" ) )
+		{
+			die ( mysql_error() );
+		}
+		else {
+			echo "success in database creation.";
+		}
+	}
+	else {
+		trigger_error ( mysql_error(), E_USER_ERROR );
+	}
+?>
