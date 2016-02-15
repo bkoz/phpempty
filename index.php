16adffc574df23d997c8d89d7ba2bee7ced6d742
Bob Kozdemba <bkozdemba@gmail.com>
2016-02-15 12:38:47 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,16 +14,23 @@
 
 	// connect to the mysql database server.
 	$connect = @mysql_connect ($host, $user, $pass);
-
+	
 	if ( $connect )
 	{
 		// create the database.
-		if ( ! @mysql_query ( "CREATE DATABASE `$database`" ) )
+		if ( ! @mysql_query ( "CREATE DATABASE $database" ) )
 		{
 			die ( mysql_error() );
 		}
 		else {
-			echo "success in database creation.";
+			mysql_select_db ( $database );
+			if ( @mysql_query ( $sql ) )
+			{
+				echo 'Your new table was created successfully!';
+			}
+			else {
+				die ( mysql_error() );
+			}
 		}
 	}
 	else {
